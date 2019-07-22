66167c6f138c68cbee742c8410f1a5aebb13eedd
Abel Ayinla <kraneabel@gmail.com>
2019-07-22 16:24:30 +0100

    Login and Dashboard Done

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const routes = express.Router();
 
+routes.get('/login', (req, res) => {
+    res.render('adminLogin');
+});
+
 routes.get('/dashboard', (req, res) => {
     res.render('admin');
-})
+});
 
 module.exports = routes;
